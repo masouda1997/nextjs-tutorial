9319b3f847e987c1a2f1b4724963ac8ae47d5e21
14022287 <anaraki.m@entekhabdigital.com>
2025-01-11 10:41:29 +0330

    color of layout

--- src/app/FAQ/layout.tsx
+++ src/app/FAQ/layout.tsx
@@ -5,7 +5,7 @@ import Loading from "./loading";
 const layout = ({children}:Readonly<{children: React.ReactNode;}>) => {
   return (<>
       <Suspense fallback ={ <Loading/>}>
-         <main className="text-center flex flex-col w-full h-[100vh] p-10 gap-5 bg-red-400">
+         <main className="text-center flex flex-col w-full h-[100vh] p-10 gap-5 bg-purple-700">
            <h1 className="font-bold text-3xl">this is FAQ layout</h1>
             {children}
          </main>
